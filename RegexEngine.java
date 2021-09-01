a705277759f86ecc8796784d2c29dde774ad333f
daolin <s4380597@uq.edu.au>
2021-09-01 10:38:18 +0930

    added genPatter, implemented + and |

--- RegexEngine.java
+++ RegexEngine.java
@@ -1,4 +1,6 @@
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 /** 
  *******************************************************************************
  * @file regular-expression-engine/RegexEngine.java
@@ -9,33 +11,55 @@
  */
 
 /**
- * regexengine
+ * regex engine
  */ 
 public class RegexEngine {
 
+    static boolean verbose = false;
     Pattern pattern;
 
     public RegexEngine(String string) {
         this.pattern = genPattern(string);
     }
 
-    public boolean match(String string){
+    public boolean match(String string) {
         String result = this.pattern.trimMatch(string);
         return result != null && result.length() == 0;
     }
 
-    public static void main(String args){
-
+    public static void main(String[] args) {
+        if (args.length == 1 && args[0].equals("-v")) {
+            verbose = true;
+        }
+        RegexEngine regexEngine = new RegexEngine("ab|c");
+        regexEngine.match("ab");
     }
 
+    //generate a state of a given regex string
     Pattern genPattern(String string) {
-
-         Pattern pattern = null;
-         
-
-
+        Pattern pattern = null;
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLetter(string.charAt(i))) {
+                int len = 1;
+                while (i + len < string.length() && Character.isLetter(string.charAt(i + len))) {
+                    len++;
+                }
+                pattern = new StringPattern(string.substring(i, i + len));
+                i += len-1;
+                continue;
+            }
+            switch (string.charAt(i)) {
+                case '+':
+                    pattern = new PlusPattern(pattern);
+                    break;
+                case '|':
+                    return new OrPattern(pattern, genPattern(string.substring(i+1)));
+                default:
+                    break;
+            }
+        }
+        return pattern;
     }
-
 
 }
 
@@ -48,7 +72,6 @@ public class RegexEngine {
 interface Pattern {
     String trimMatch(String string);
 }
-
 
 class StringPattern implements Pattern {
     String string;
@@ -63,5 +86,43 @@ class StringPattern implements Pattern {
             return string.substring(this.string.length());
         }
         return null;
+    }
+}
+
+class PlusPattern implements Pattern {
+    Pattern pattern;
+
+    public PlusPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    @Override
+    public String trimMatch(String string) {
+        String rest = pattern.trimMatch(string);
+        if (rest != null) {
+            String result;
+            do {
+                result = rest;
+                rest = pattern.trimMatch(rest);
+            } while (rest != null);
+            return result;
+        }
+        return null;
+    }
+}
+
+class OrPattern implements Pattern {
+    Pattern left, right;
+
+    public OrPattern(Pattern left, Pattern right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String trimMatch(String string) {
+        String left = this.left.trimMatch(string);
+        String right = this.right.trimMatch(string);
+        return left == null ? right:left;
     }
 }
